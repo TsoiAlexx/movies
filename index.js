3ee48992402b8582d007a9601386e8502d068528
Aleksei Tsoi <tsoialeksei17@gmail.com>
2026-01-14 10:30:37 +0300

    movie detail

--- index.js
+++ index.js
@@ -30,18 +30,65 @@ const resultsWrapper = document.querySelector(".results");
 const onInput = async (event) => {
   const movies = await fetchData(event.target.value);
 
+  if (!movies.length) {
+    dropdown.classList.remove("is-active");
+  }
+
+  resultsWrapper.innerHTML = "";
+
   dropdown.classList.add("is-active");
 
   for (let movie of movies) {
     const option = document.createElement("a");
+    const imgSRC = movie.Poster === "N/A" ? "" : movie.Poster;
 
     option.classList.add("dropdown-item");
     option.innerHTML = `
-      <img src="${movie.Poster}" />
+      <img src="${imgSRC}" />
       ${movie.Title}
       `;
+
+    option.addEventListener("click", (event) => {
+      dropdown.classList.remove("is-active");
+      input.value = movie.Title;
+      onMovieSelect(movie);
+    });
+
     resultsWrapper.appendChild(option);
   }
 };
 
-input.addEventListener("input", debounce(onInput, 3000));
+input.addEventListener("input", debounce(onInput, 1000));
+
+document.addEventListener("click", (event) => {
+  if (!root.contains(event.target)) {
+    dropdown.classList.remove("is-active");
+  }
+});
+
+const onMovieSelect = async (movie) => {
+  const response = await axios.get("http://www.omdbapi.com/", {
+    params: {
+      apikey: "7f98fc67",
+      i: movie.imdbID,
+    },
+  });
+  document.querySelector("#summary").innerHTML = movieTemplate(response.data);
+};
+
+const movieTemplate = (movieDetail) => {
+  return `
+    <article class="media">
+      <figure class="media-left">
+        <p class="image">
+          <img src="${movieDetail.Poster}" />
+        </p>
+      </figure>
+      <div class="content">
+        <h1>${movieDetail.Title}</h1>
+        <h4>${movieDetail.Genre}</h4>
+        <p>${movieDetail.Plot}</p>
+      <div>
+    </article>
+  `;
+};
